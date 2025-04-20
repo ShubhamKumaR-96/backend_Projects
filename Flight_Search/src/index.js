7923ef2c34c462d2fa9252dd18fcf7b0677932d6
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-04-20 10:08:20 +0530

    complete basic sequelize setup for create DB

--- Flight_Search/src/index.js
+++ Flight_Search/src/index.js
@@ -3,7 +3,8 @@ const { PORT } = require("./config/serverConfig");
 
 
 const app=express()
+app.use(express.json())
 
-app.listen(PORT ,() => {
+app.listen(PORT,() => {
     console.log(`Server running on http://localhost:${PORT}`)
   });
